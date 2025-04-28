cd41607fc4d124c2e866fada9304fe3c685c668e
kngruber <kngruber@my.waketech.edu>
2025-04-28 17:51:22 -0400

    Updated DebugJS file

---
+++ gradebook.js
@@ -0,0 +1,14 @@
+//TODO: Fetch data from PostgreSQL database 
+function fetchGradeData() {
+    console.log("Fetching grade data...");
+}
+
+//TODO: Populate table with grade data
+function populateGradeBook(data) {
+    console.log("Populating gradebook with data:", data);
+}
+
+//TODO: REMOVE THIS
+//TODO: Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradeBook(gradeData);
